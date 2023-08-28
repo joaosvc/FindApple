f1499351c94decf2382500b8cde998f082c2633a
João <102983598+joaosvc@users.noreply.github.com>
2023-08-28 07:07:39 -0300

    Update AuthScreen.tsx

--- components/auth/AuthScreen.tsx
+++ components/auth/AuthScreen.tsx
@@ -4,7 +4,7 @@ const AuthScreen = async () => {
   return (
     <div className="w-screen h-full flex flex-row items-center justify-center">
       <div className="flex h-auto flex-row p-6">
-        <div className="w-[20rem] sm:w-[22rem] h-full p-6 flex flex-col items-center justify-center rounded-md shadow-md">
+        <div className="w-[20rem] sm:w-[22rem] h-[90vh] p-6 flex flex-col items-center justify-center rounded-md shadow-md">
           <h1 className="text-2xl font-extrabold mb-12">Auth</h1>
           <AuthForm />
         </div>

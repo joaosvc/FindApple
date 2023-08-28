0e477293ca7031377327809626ccda3cbd520831
Joaosvc <joaosvc.dev@gmail.com>
2023-08-27 23:36:44 -0300

    Changing production mode

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,7 @@ declare global {
 }
 
 let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   prisma = new PrismaClient()
 } else {
   if (!global.cachedPrisma) {

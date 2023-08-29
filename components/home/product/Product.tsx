ad3c2eea4a205bcf417c3557760f022fda611615
João <102983598+joaosvc@users.noreply.github.com>
2023-08-28 22:15:30 -0300

    Update Product.tsx

--- components/home/product/Product.tsx
+++ components/home/product/Product.tsx
@@ -8,7 +8,7 @@ interface IProduct {
 
 const Product = ({ name, icon, type }: IProduct) => {
   return (
-    <Link href={`devices/${type}`}>
+    <Link href={`device/${type}`}>
       <div className="flex flex-col items-center space-y-3 transition duration-150 hover:drop-shadow-md cursor-pointer">
         <img className="h-[4rem] sm:h-[5rem]" src={icon} alt={name} />
         <span className="font-medium">{name}</span>
